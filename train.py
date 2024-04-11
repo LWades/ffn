7632411def3578a316fa074666e271ba56bec209
lwades <601149711@qq.com>
2024-04-12 04:08:09 +0800

    dl

--- train.py
+++ train.py
@@ -101,6 +101,54 @@ def valid_1d(model, test_loader):
     return accuracy
 
 
+def valid_1d_eval(model, test_loader):
+    # Validation!
+    eval_losses = AverageMeter()
+    model.eval()
+    all_preds, all_label = [], []
+    epoch_iterator = tqdm(test_loader,
+                          desc="Validating... (loss=X.X)",
+                          bar_format="{l_bar}{r_bar}",
+                          dynamic_ncols=True)
+    loss_fct = torch.nn.CrossEntropyLoss()
+    for step, batch in enumerate(epoch_iterator):
+        batch = tuple(t.to(device) for t in batch)
+        x, y = batch
+        # x = x.to(torch.float16)
+        # y = y.to(torch.long)
+        y = torch.squeeze(y)
+        y = y.to(torch.long)
+        with torch.no_grad():
+            logits = model(x)
+            # logits = model(x)[0]
+            # log(f"logits shape: {logits.shape}")
+            # log(f"y shape: {y.shape}")
+
+            eval_loss = loss_fct(logits, y)
+            eval_losses.update(eval_loss.item())
+
+            preds = torch.argmax(logits, dim=-1)
+
+        if len(all_preds) == 0:
+            all_preds.append(preds.detach().cpu().numpy())
+            all_label.append(y.detach().cpu().numpy())
+        else:
+            all_preds[0] = np.append(
+                all_preds[0], preds.detach().cpu().numpy(), axis=0
+            )
+            all_label[0] = np.append(
+                all_label[0], y.detach().cpu().numpy(), axis=0
+            )
+        epoch_iterator.set_description("Validating... (loss=%2.5f)" % eval_losses.val)
+
+    all_preds, all_label = all_preds[0], all_label[0]
+    accuracy = (all_preds == all_label).mean()
+
+    log("Valid Loss: %2.5f" % eval_losses.avg)
+    log("Valid Accuracy: %2.5f" % accuracy)
+
+    return accuracy
+
 def valid(model, test_loader):
     # Validation!
     eval_losses = AverageMeter()
